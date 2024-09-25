c5bc4473485c52f64b675d2da7fb0819778a2be3
bshongwe <dru.da.beat.hustle@gmail.com>
2024-09-25 21:33:48 +0200

    Blog app landing components

---
+++ src/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <h1>Blogit</h1>
+            <p>&copy; Copyright 2024 All Rights Reserved.</p>
+        </div>
+    )
+}
+
+export default Footer
